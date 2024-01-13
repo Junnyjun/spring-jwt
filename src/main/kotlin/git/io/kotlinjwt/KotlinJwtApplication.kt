198a4d39e475134d583679c97453fca10dc7154e
junny <chbe5082@naver.com>
2024-01-13 13:48:09 +0900

    default setup

--- src/main/kotlin/git/io/kotlinjwt/KotlinJwtApplication.kt
+++ src/main/kotlin/git/io/kotlinjwt/KotlinJwtApplication.kt
@@ -3,6 +3,7 @@ package git.io.kotlinjwt
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class KotlinJwtApplication
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/git/io/kotlinjwt/security/config/SecurityConfiguration.kt
@@ -0,0 +1,10 @@
+package git.io.kotlinjwt.security.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+
+}
