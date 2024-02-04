bf296b9f63e0b8823471212e74368a85be1b0bc9
junny <chbe5082@naver.com>
2024-02-04 21:25:38 +0900

    chap2

--- src/main/kotlin/git/io/kotlinjwt/security/config/SecurityConfiguration.kt
+++ src/main/kotlin/git/io/kotlinjwt/security/config/SecurityConfiguration.kt
@@ -2,29 +2,35 @@ package git.io.kotlinjwt.security.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.ObjectPostProcessor
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 
+
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration(
+    private val objectPostProcessor: ObjectPostProcessor<Any>
+) {
+
     @Bean
-    fun passwordEncoder():PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    fun passwordEncoder(): PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain = http
-        .requiresChannel { it.anyRequest().requiresSecure() }
-        .anonymous { it.disable() }
-        .csrf { it.disable() }
-        .authenticationManager{
-            it
-        }
-        .cors { it.configurationSource { corsConfiguration() } }
-        .portMapper { it.http(8080).mapsTo(8443) }
+        .httpBasic { }
+        .authorizeHttpRequests { it.anyRequest().authenticated() }
+        .authenticationManager(authenticationManager(AuthenticationManagerBuilder(objectPostProcessor)))
+        .authenticationProvider()
         .build()
 
     private fun corsConfiguration(): CorsConfiguration = CorsConfiguration().apply {
@@ -34,4 +40,18 @@ class SecurityConfiguration {
         allowCredentials = true
     }
 
+    fun userDetailsService(): UserDetailsService = InMemoryUserDetailsManager()
+        .also {
+            it.createUser(
+                User
+                    .withUsername("junnyland").password("1234").roles("USER").build()
+            )
+        }
+    fun authenticationManager(auth: AuthenticationManagerBuilder): AuthenticationManager = auth
+            .also {
+                it
+                    .userDetailsService<UserDetailsService>(userDetailsService())
+                    .passwordEncoder(passwordEncoder())
+            }
+            .build()
 }
---
+++ src/test/http/curl.sh
@@ -0,0 +1,1 @@
+curl -u junnyland:1234 -k https://localhost:8080/hello
